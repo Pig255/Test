77d00c27ed303820227be7fc8f52b6f6342972ee
翟怡丰 <1157223595@qq.com>
2022-07-11 02:19:09 +0800

    新改了一版本

---
+++ cese/src/hello.java
@@ -0,0 +1,10 @@
+/**
+ * @author: Jerry_Z
+ * @date: 2022/7/11 2:07
+ * @description:
+ */
+public class hello {
+    public static void main(String args[]) {
+        System.out.println("helloWorld");
+    }
+}
